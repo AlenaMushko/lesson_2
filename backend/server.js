7450ba86ef7381196354c00c10740ee32dbad355
maxyma-sanx <maxymasan.dev@gmail.com>
2023-06-02 20:33:41 +0300

    add emailservice

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,59 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const path = require("path");
-require("colors");
-const connectDb = require("../config/db");
-const bcrypt = require("bcrypt");
-const errorHandler = require("./midlewares/errorHandler");
-const asyncHandler = require("express-async-handler");
-const authMidleware = require("./midlewares/authMidleware");
-const RoleModel = require("./models/RoleModel");
+const express = require('express');
+const { engine } = require('express-handlebars');
+const dotenv = require('dotenv');
+const path = require('path');
+require('colors');
+const connectDb = require('../config/db');
+const bcrypt = require('bcrypt');
+const errorHandler = require('./midlewares/errorHandler');
+const asyncHandler = require('express-async-handler');
+const authMidleware = require('./midlewares/authMidleware');
+const RoleModel = require('./models/RoleModel');
 
-const configPath = path.join(__dirname, "..", "config", ".env");
+const configPath = path.join(__dirname, '..', 'config', '.env');
 dotenv.config({ path: configPath });
-const UserModel = require("./models/UsersModel");
-const jwt = require("jsonwebtoken");
+const UserModel = require('./models/UsersModel');
+const jwt = require('jsonwebtoken');
+const sendEmail = require('./services/emailService');
 
 const app = express();
+
+// Set template engine
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', 'backend/views');
+
 app.use(express.urlencoded({ extended: false })); // обробляти форми
 app.use(express.json()); // як і app.use(express.urlencoded({extended: false})), але для json
 
-app.use("/api/v1", require("./routes/moviesRouter")); //вказуємо що будемо виконувати і де всі роути прописані
+app.use('/api/v1', require('./routes/moviesRouter')); //вказуємо що будемо виконувати і де всі роути прописані
+
+// Set routes
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.post('/send', async (req, res) => {
+  try {
+    res.render('send', {
+      name: req.body.userName,
+      email: req.body.userEmail,
+      message: 'success',
+    });
+
+    await sendEmail(req.body);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 //реєстрація - це збереження користувача в базі данних
 //аутентифікація - це перевірка данних, які надав користувач з тим, що є в базі данних
@@ -26,9 +61,9 @@ app.use("/api/v1", require("./routes/moviesRouter")); //вказуємо що б
 //logOut - вихід із застосунку
 
 app.post(
-  "/register",
+  '/register',
   (req, res, next) => {
-    console.log("Спрацював ДЖОИ");
+    console.log('Спрацював ДЖОИ');
     next();
   },
   asyncHandler(async (req, res) => {
@@ -37,7 +72,7 @@ app.post(
     //2.Валідація отриманних даних
     if (!email || !password) {
       res.status(400);
-      throw new Error("provide all required fields");
+      throw new Error('provide all required fields');
     }
     //3.Перевірка чи є користувач у базі данних
     const candidat = await UserModel.findOne({ email });
@@ -45,11 +80,11 @@ app.post(
     //4.Якщо знайшли, повідомляємо "Такий користувач вже існує"
     if (candidat) {
       res.status(400);
-      throw new Error("User already exists");
+      throw new Error('User already exists');
     }
     //5.Якщо не знайшли - хешуємо пароль
     const hashPassword = bcrypt.hashSync(password, 5);
-    const userRole = await RoleModel.findOne({ value: "ADMIN" });
+    const userRole = await RoleModel.findOne({ value: 'ADMIN' });
     //6.Зберігаємо користувача у базі даних
     const newUser = await UserModel.create({
       ...req.body,
@@ -58,7 +93,7 @@ app.post(
     });
     if (!newUser) {
       res.status(409);
-      throw new Error("Unable to save database");
+      throw new Error('Unable to save database');
     }
     return res
       .status(201)
@@ -67,7 +102,7 @@ app.post(
 );
 
 app.post(
-  "/login",
+  '/login',
   asyncHandler(async (req, res) => {
     //1.Отримання даних від користувача
     const { email, password } = req.body;
@@ -75,7 +110,7 @@ app.post(
     //2.Валідація отриманних даних
     if (!email || !password) {
       res.status(400);
-      throw new Error("provide all required fields");
+      throw new Error('provide all required fields');
     }
     //3.Шукаємо користувача по имейлу
     const user = await UserModel.findOne({ email });
@@ -84,17 +119,17 @@ app.post(
     const isValidPassword = bcrypt.compareSync(password, user.password);
     if (!user || !isValidPassword) {
       res.status(400);
-      throw new Error("Invalid login or password");
+      throw new Error('Invalid login or password');
     }
     //5. Якщо знайшли і розшифровали пароль то видаєм токен
-    const friends = ["pasha", "oleg", "andriy", "alyona", "julia"];
+    const friends = ['pasha', 'oleg', 'andriy', 'alyona', 'julia'];
 
     user.token = generateToken({ friends, id: user._id });
 
     const userWithToken = await user.save();
     if (!userWithToken) {
       res.status(400);
-      throw new Error("Unable to save token");
+      throw new Error('Unable to save token');
     }
     return res
       .status(200)
@@ -104,26 +139,26 @@ app.post(
 );
 
 app.get(
-  "/logout",
+  '/logout',
   authMidleware,
   asyncHandler(async (req, res) => {
     const id = req.user;
     await UserModel.findByIdAndUpdate(id, { token: null });
-    res.json({ message: "logout succes" });
+    res.json({ message: 'logout succes' });
   })
 );
 
 function generateToken(data) {
   const payload = { ...data };
-  return jwt.sign(payload, "pizza", { expiresIn: "5h" });
+  return jwt.sign(payload, 'pizza', { expiresIn: '5h' });
 }
 
-app.use("*", (req, res, next) => {
+app.use('*', (req, res, next) => {
   //якщо невірна адреса
   res.status(404);
   res.json({
     code: 404,
-    message: "not found",
+    message: 'not found',
   });
   next();
 });
---
+++ backend/services/emailService.js
@@ -0,0 +1,40 @@
+'use strict';
+const nodemailer = require('nodemailer');
+
+async function sendEmail(data) {
+  const { userName, userEmail, userMessage } = data;
+
+  let transporter = nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'max_goit_63@outlook.com', // generated ethereal user
+      pass: '63_max_goit', // generated ethereal password
+    },
+  });
+
+  const emailContent = `<h1 style="color: green">Ви отримали листа! Від ${userName}</h1>
+  <p>Він написав таке ${userMessage} повідомлення.</p>
+  <p>Його контактний email ${userEmail}</p>
+  <p style="color: blue">
+    Вітаю ви отримали листа від компанії GoIT. Купіть 50 сковородок!
+  </p>`;
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: 'max_goit_63@outlook.com', // sender address
+    to: 'maxymasan.dev@gmail.com', // list of receivers
+    subject:
+      'City of Yellow Waters, Three Horses Monument, July 4, 7:00 p.m., First Galactic Conference', // Subject line
+    text: userMessage, // plain text body
+    html: emailContent, // html body
+  });
+
+  console.log('Message sent: %s', info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  return true;
+}
+
+module.exports = sendEmail;
